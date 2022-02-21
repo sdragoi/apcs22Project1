7c1153edc221006c72fbedc93b8057a2ac1289a9
sdragoi <sdragoi24@cathedral.org>
2022-02-21 13:09:02 -0500

    Update Tester.java
    Added pa

--- Tester.java
+++ Tester.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Integer k = 123812;
-		String j = k.toString();
-		System.out.println(j);
-
+		Scanner input = new Scanner(System.in);
+		
+		int runs;
+		do {
+			System.out.print("Please specify either 5, 10, 50, or 100 million runs by inputting 5, 10, 50, or 100. ");
+			if (!(input.nextInt() == 5 || input.nextInt() == 10 || input.nextInt() == 50 || input.nextInt() == 100)) {
+				System.out.println("Not within the specified parameters.");
+			}
+		}
+		while (!(input.hasNextInt()));
+		
+		runs = input.nextInt();
+		
+		int k = input.nextInt();
+		System.out.println("Your input: " + k);
 		
 
 	}
