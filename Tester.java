d2804d367b580518ece3ee4662496c02d8cfadaf
sdragoi <sdragoi24@cathedral.org>
2022-02-22 08:41:49 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,29 +4,33 @@ public class Tester {
 
 	public static void main(String[] args) {
 		
-		try (Scanner input = new Scanner(System.in)) {
-			int runs;
+		System.out.print("Please specify either 5, 10, 50, or 100 million runs by inputting 5, 10, 50, or 100. ");
+		
+		int runs;
+		Scanner input = new Scanner(System.in);
+		if (input.hasNextInt()) {
 			switch (input.nextInt()){
-				case 5:	
-				case 10:
-				case 50:
-				case 100:
-					runs = input.nextInt();
-				default:
-					System.out.println("Please enter a valid input.");
+			case 5:	
+			case 10:
+			case 50:
+			case 100:
+				runs = input.nextInt();
+			default:
+				System.out.println("Please enter a valid input.");
 			
-			}
-			
-			
-			
-				System.out.print("Please specify either 5, 10, 50, or 100 million runs by inputting 5, 10, 50, or 100. ");
-
-			
-
-
-			int k = input.nextInt();
-			System.out.println("Your input: " + k);
 		}
+		
+		
+		}
+		
+		
+	
+	
+		
+	
+
+		int k = input.nextInt();
+		System.out.println("Your input: " + k);
 		
 
 	}
