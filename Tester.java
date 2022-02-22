e32e103e6f08456166a07410b2fa2f110ce86697
sdragoi <sdragoi24@cathedral.org>
2022-02-22 08:26:03 -0500

    Update Tester.java
    Scanner object edits

--- Tester.java
+++ Tester.java
@@ -4,16 +4,17 @@ public class Tester {
 
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
+		Scanner input;
 		
-		int runs;
-		System.out.print("Please specify either 5, 10, 50, or 100 million runs by inputting 5, 10, 50, or 100. ");
-		if (!(input.nextInt() == 5 || input.nextInt() == 10 || input.nextInt() == 50 || input.nextInt() == 100)) {
-			System.out.println("Not within the specified parameters.");
+		do {
+			System.out.print("Please specify either 5, 10, 50, or 100 million runs by inputting 5, 10, 50, or 100. ");
+			input = new Scanner(System.in);
 		}
-
+		while (!(input.nextInt() == 5 || input.nextInt() == 10 || input.nextInt() == 50 || input.nextInt() == 100));
 		
-		runs = input.nextInt();
+	
+		
+		int runs = input.nextInt();
 		
 		int k = input.nextInt();
 		System.out.println("Your input: " + k);
